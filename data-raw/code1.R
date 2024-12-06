bc1a2b3c6522f9cddf4b1115e7b76cf93136c579
Ioannis Malagaris <ymalag@gmail.com>
2024-12-06 11:23:33 -0600

    a

--- data-raw/code1.R
+++ data-raw/code1.R
@@ -24,28 +24,31 @@ aek <- function(x, y, z) {
   sys.call()
 }
 
-scenarios$sc3$plot_$initArgs |>
-  (\(x) names(x) |> lapply(\(y) paste0(y, " = ", x[[y]])) )() |>
-  unlist() |>
-  paste0(collapse = ", ") |>
-  paste0("(", ... = _, ")")
-
-
-
-
-LoadData(gripsYR1, ScreenDt, Enrolled)
-(res <- GetWeekPredCI(fillGaps = TRUE, coeff = 1.5))
-scenarios <- list(
-  sc1 = GetWeekPredCI(),
-  sc2 = GetWeekPredCI(cauchyWt = TRUE),
-  sc3 = GetWeekPredCI(fillGaps = TRUE),
-  sc4 = GetWeekPredCI(fillGaps = TRUE, coeff = 1.2)
-)
-maxY <- sapply(scenarios, \(x) x$plot_$maxY) |> max()
-
-graphics::par(mfrow = c(2, 2))
-for (x in scenarios) x$predPlot(yMax = maxY, argsAsTitle = T)
-
-a <- function(x =5, y = 6) {
-  sys.call()
+bQ <- \(x) substitute(bquote(y), list(y = x)) |> eval() |> as.character()
+bQstr <- \(x) (if (is.character(x)) str2lang(x) else x) |> bQ()
+unQ <- \(x) vapply(x, bQstr, "")
+ss <- \(x) {
+  p <- "\\.\\(([^()]*(?:\\((?:[^()]*(?:\\([^()]*\\))*[^()]*)*\\))?[^()]*)*\\)"
+  m <- gregexpr(p, x, perl = TRUE)
+  if (m[[1L]][[1L]] == -1) return(x)
+  regmatches(x, m) <- regmatches(x, m) |> lapply(unQ)
+  x
 }
+
+
+mySwitch <- \(x) {
+  switch(
+    typeof(x),
+    character = x,
+    symbol = deparse(x),
+    language = unQ(x)
+  )
+}
+
+fn <- \(...) {
+  cargs <- as.list(substitute(...()))
+  lapply(cargs, mySwitch)
+}
+
+
+
